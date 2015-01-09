9bf871381d3914d0d3ef6bff59743fea15db5e11
snehasishmohanta <snehasishmohanta@gmail.com>
2015-01-09 15:42:47 +0530

    Update retrieve_json_folder.py

--- retrieve_json_folder.py
+++ retrieve_json_folder.py
@@ -1,3 +1,27 @@
+
+#it will print all the data 
+
+import re
+import os
+import json
+auto_prefix = u'\u200B'
+path = "/home/snehasish/log/search1/"
+
+for path, dirs, files in os.walk(path):
+        for data in files:
+                with open("/home/snehasish/log/search1/"+data) as text:
+                        for line in text:
+                                 file1 = json.loads(line)
+                                 try:
+                                        query = (file1["appData"]["_request_params"]["params"]["q"])[0]
+
+                                        query = query.replace(auto_prefix, "")
+                                        print file1["requestDate"],"-",query.encode('utf-8')
+                                 except KeyError:
+                                        pass
+
+#below code will print all the value which contain unicode infront of query.
+
 import re
 import json
 import sys
