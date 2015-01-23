9caff8ac4db1c56af21997b1d94203c27fbf122e
Snehasish Mohanta <snehasishmohanta@gmail.com>
2015-01-23 09:23:12 +0530

    Update retrieve_json_folder.py

--- retrieve_json_folder.py
+++ retrieve_json_folder.py
@@ -77,3 +77,71 @@ for path, dirs, files in os.walk(path2):
                                                 print file1["requestDate"],",",query.encode('utf-8'), ", FALSE"
                                 except KeyError:
                                         pass
+# remove if page having value more than 1
+
+import re
+import os
+import json
+auto_prefix = u'\u200B'
+path1 = "/home/snehasish/new-log/1-search-logs-01-20/"
+path2 = "/home/snehasish/new-log/2-search-logs-01-20/"
+for path, dirs, files in os.walk(path1):
+	for file1 in files:
+        	with open("/home/snehasish/new-log/1-search-logs-01-20/"+file1) as text:
+                	for line in text:
+				data = json.loads(line)
+                		if data["appData"]["_request_params"]["params"].has_key("page"):
+                        		page = (data["appData"]["_request_params"]["params"]["page"])[0]
+                        		if page == "1":
+                                		try:
+                                        		query = (data["appData"]["_request_params"]["params"]["q"])[0]                                          
+                                        		if auto_prefix in query:
+                                                		query = query.replace(auto_prefix, "")
+                                                		print data["requestDate"],",",query.encode('utf-8'), ", TRUE"
+                                        		else:
+                                                		print data["requestDate"],",",query.encode('utf-8'), ", FALSE"
+                                		except KeyError:
+                                        		pass
+
+                        		else:
+                                		pass
+                		else:
+                        		try:
+                                		query = (data["appData"]["_request_params"]["params"]["q"])[0]
+                                		if auto_prefix in query:
+                                	        	query = query.replace(auto_prefix, "")
+                                        		print data["requestDate"],",",query.encode('utf-8'), ", TRUE"
+                                		else:
+                                        		print data["requestDate"],",",query.encode('utf-8'), ", FALSE"
+                        		except KeyError:
+                                		pass
+for path, dirs, files in os.walk(path2):
+        for file1 in files:
+                with open("/home/snehasish/new-log/2-search-logs-01-20/"+file1) as text:
+                        for line in text:
+                                data = json.loads(line)
+                                if data["appData"]["_request_params"]["params"].has_key("page"):
+                                        page = (data["appData"]["_request_params"]["params"]["page"])[0]
+                                        if page == "1":
+                                                try:
+                                                        query = (data["appData"]["_request_params"]["params"]["q"])[0]                          
+                                                        if auto_prefix in query:
+                                                                query = query.replace(auto_prefix, "")
+                                                                print data["requestDate"],",",query.encode('utf-8'), ", TRUE"
+                                                        else:
+                                                                print data["requestDate"],",",query.encode('utf-8'), ", FALSE"
+                                                except KeyError:
+                                                        pass
+
+                                        else:
+                                                pass
+                                else:
+                                        try:
+                                                query = (data["appData"]["_request_params"]["params"]["q"])[0]
+                                                if auto_prefix in query:
+                                                        query = query.replace(auto_prefix, "")
+                                                        print data["requestDate"],",",query.encode('utf-8'), ", TRUE"
+                                                else:
+                                                        print data["requestDate"],",",query.encode('utf-8'), ", FALSE"
+                                        except KeyError:
+                                                pass
